e7e91253c2e15ed97980ad8ddb4e56bc38c94d81
Ankit Sharma <buddingintents@google.com>
2023-03-19 22:07:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using OpenAI_API;
+using OpenAI_API.Chat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenAIIntegration
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            bool exitNow = false;
+            OpenAIAPI api = new OpenAIAPI(@"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");
+            string response;
+            var chat = api.Chat.CreateConversation();
+            string readResponse;
+
+            /// give instruction as System
+            Console.WriteLine("If you have System message. Press Y else any other key");
+            readResponse = Console.ReadLine().ToString();
+            if (readResponse.ToUpper() == "Y")
+            {
+                Console.Write("system:");
+                chat.AppendSystemMessage(Console.ReadLine());
+                Console.WriteLine($"{chat.Messages[0].Role}: {chat.Messages[0].Content}");
+            }
+            while (!exitNow)
+            {
+                Console.Write("USER:");
+                chat.AppendUserInput(Console.ReadLine());
+                response = await chat.GetResponseFromChatbot();
+                chat.AppendExampleChatbotOutput(response);
+                Console.WriteLine($"{chat.Messages[chat.Messages.Count - 1].Role}: {chat.Messages[chat.Messages.Count - 1].Content}");
+                Console.WriteLine("Press X to exit");
+                readResponse = Console.ReadLine().ToString();
+                if (readResponse.ToUpper() == "X")
+                    exitNow = true;
+            }
+        }
+    }
+}
